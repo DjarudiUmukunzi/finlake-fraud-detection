fd45d50de9edf96bc48ed01923e964df316b8fc4
djarudi <djarudimukunzi@outlook.com>
2025-10-08 00:45:43 +0500

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,6 +1,4 @@
-variable "rg_name"    { default = "rg-finlake-dev" }
-variable "location"   { default = "East US" }
-variable "sa_prefix"  { default = "finlakeadls" }
-variable "kv_name"    { default = "kv-finlake-dev" }
-variable "adf_name"   { default = "adf-finlake-dev" }
-variable "dbw_name"   { default = "databricks-finlake-dev" }
+variable "ARM_CLIENT_ID" {}
+variable "ARM_CLIENT_SECRET" {}
+variable "ARM_TENANT_ID" {}
+variable "ARM_SUBSCRIPTION_ID" {}
