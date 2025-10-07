918401405d07562c80242650e0b961b78e7927e0
djarudi <djarudimukunzi@outlook.com>
2025-10-08 00:49:58 +0500

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,4 +1,29 @@
-variable "ARM_CLIENT_ID" {}
-variable "ARM_CLIENT_SECRET" {}
-variable "ARM_TENANT_ID" {}
-variable "ARM_SUBSCRIPTION_ID" {}
+variable "rg_name" {
+  description = "Name of the Azure Resource Group"
+  default     = "rg-finlake-dev"
+}
+
+variable "location" {
+  description = "Azure region for all resources"
+  default     = "East US"
+}
+
+variable "sa_prefix" {
+  description = "Prefix for the storage account name"
+  default     = "finlakeadls"
+}
+
+variable "kv_name" {
+  description = "Name of the Key Vault"
+  default     = "kv-finlake-dev"
+}
+
+variable "adf_name" {
+  description = "Name of the Azure Data Factory"
+  default     = "adf-finlake-dev"
+}
+
+variable "dbw_name" {
+  description = "Name of the Databricks workspace"
+  default     = "databricks-finlake-dev"
+}
