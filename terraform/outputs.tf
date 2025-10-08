7120f132e5cf5343be957510bedfb124e7789c05
djarudi <djarudimukunzi@outlook.com>
2025-10-08 15:53:12 +0500

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,15 +1,24 @@
-output "resource_group_name" {
-  value = azurerm_resource_group.rg.name
+output "resource_group" {
+  description = "Name of the resource group"
+  value       = azurerm_resource_group.rg.name
 }
-output "storage_account_name" {
-  value = azurerm_storage_account.adls.name
+
+output "storage_account" {
+  description = "ADLS Gen2 storage account name"
+  value       = azurerm_storage_account.adls.name
 }
-output "key_vault_name" {
-  value = azurerm_key_vault.kv.name
+
+output "key_vault" {
+  description = "Key Vault name"
+  value       = azurerm_key_vault.kv.name
 }
-output "adf_name" {
-  value = azurerm_data_factory.adf.name
+
+output "data_factory" {
+  description = "Data Factory name"
+  value       = azurerm_data_factory.adf.name
 }
-output "databricks_workspace_name" {
-  value = azurerm_databricks_workspace.dbw.name
+
+output "databricks_workspace" {
+  description = "Databricks workspace name"
+  value       = azurerm_databricks_workspace.dbw.name
 }
